3b530f8fecab8bb5004520269bb8664a30f0297f
mikiyas2196 <mikiiyassebsibe@gmail.com>
2025-07-07 12:32:38 -0700

    Banner created

--- netflixclone/src/components/header/Header.js
+++ netflixclone/src/components/header/Header.js
@@ -1,39 +1,54 @@
-import React from 'react'
-import "./header.css"
-import Netflix_logo from "../../assets/images/Netflix_logo.svg"
-import abika_logo from "../../assets/images/abika_logo.png"
-
-import SearchIcon from '@mui/icons-material/Search';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
-import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import React, { useEffect, useState } from "react";
+import "./header.css";
+import Netflix_logo from "../../assets/images/Netflix_logo.svg";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import PersonIcon from "@mui/icons-material/Person";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
 function Header() {
+  const [showBackground, setShowBackground] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setShowBackground(true);
+      } else {
+        setShowBackground(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <div className='header_outer_container'>
-      <div className='header_container'>
-         <div className='header_left'>
-            <ul>
-                <li><img src={Netflix_logo} alt='Netflix logo' className='net_logo'/></li>
-                <li>Home</li>
-                <li>TVShows</li>
-                <li>Movies</li>
-                <li>Latest</li>
-                <li>MyList</li>
-                <li>Browse by Languages</li>
-            </ul>
-         </div>
-         <div className='header_right'>
-            <ul>
-                <li><SearchIcon className='icon' /></li>
-                <li><NotificationsIcon className='icon' /></li>
-                <li><AccountBoxIcon className='icon' /></li>
-                <li><ArrowDownwardIcon className='icon' /></li>
-            </ul>
-         </div>
+    <div className={`header_outer_container ${showBackground ? "scrolled" : ""}`}>
+      <div className="header_container">
+        <div className="header_left">
+          <ul>
+            <li>
+              <img src={Netflix_logo} alt="Netflix Logo" className="net_logo" />
+            </li>
+            <li>Home</li>
+            <li>TVShows</li>
+            <li>Movies</li>
+            <li>Latest</li>
+            <li>MyList</li>
+            <li>Browse by Languages</li>
+          </ul>
+        </div>
+        <div className="header_right">
+          <ul>
+            <li><SearchIcon className="icon" /></li>
+            <li><NotificationsIcon className="icon" /></li>
+            <li><PersonIcon className="icon" /></li>
+            <li><ArrowDropDownIcon className="icon" /></li>
+          </ul>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- netflixclone/src/pages/Home.js
+++ netflixclone/src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../components/header/Header'
 import Footer from '../components/footer/Footer'
+import Banner from '../components/Banner/Banner'
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Banner />
       <Footer />
     </>
   )
